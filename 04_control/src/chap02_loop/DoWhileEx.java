7a0d0901f21c898063551590e331b2fcc329bb6e
jinseong <jinseongpark1212@gmail.com>
2025-06-27 10:59:04 +0900

    20250627

---
+++ 04_control/src/chap02_loop/DoWhileEx.java
@@ -0,0 +1,10 @@
+package chap02_loop;
+
+public class DoWhileEx {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ 04_control/src/chap02_loop/WhileEx.java
@@ -0,0 +1,10 @@
+package chap02_loop;
+
+public class WhileEx {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
