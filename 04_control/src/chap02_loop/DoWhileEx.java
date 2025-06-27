5848f26dcb961f18770247af90959402a3b6f813
jinseong <jinseongpark1212@gmail.com>
2025-06-27 12:44:47 +0900

    20250627

--- 04_control/src/chap02_loop/DoWhileEx.java
+++ 04_control/src/chap02_loop/DoWhileEx.java
@@ -3,7 +3,17 @@ package chap02_loop;
 public class DoWhileEx {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    
+    // 1 ~ 100 누적하기
+    
+    int total = 0;
+    int x = 1;
+    
+    do {
+      total += x++;
+    } while(x <= 100);
+    
+    System.out.println(total);
 
   }
 
